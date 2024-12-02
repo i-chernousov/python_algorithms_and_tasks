db092bd0e2f97c62af2ebf2470a9a544f331bca1
i-chernousov <138145388+i-chernousov@users.noreply.github.com>
2024-12-02 23:20:36 +0300

    Create 1.py

---
+++ tasks/project_euler/1.py
@@ -0,0 +1,24 @@
+"""
+Если выписать все натуральные числа меньше 10, кратные 3 или 5, то получим 3, 5, 6 и 9. Сумма этих чисел равна 23.
+Найдите сумму всех чисел меньше 1000, кратных 3 или 5.
+"""
+
+def result(multiple_number_one: int, multiple_number_two: int, natural_numbers: int):
+    """
+    Функция получает на вход значения и находит сумму кратных чисел
+    :param multiple_number_one: - кратное число № 1
+    :param multiple_number_two: - кратное число № 2
+    :param natural_numbers: - натуральное число
+    :return: сумму кратных чисел меньше натурального числа
+    """
+    sum_number = 0
+
+    for i in range(1, natural_numbers):
+        if i % multiple_number_one == 0 or i % multiple_number_two == 0:
+            sum_number += i
+    else:
+        return sum_number # 233168
+
+if __name__ == "__main__":
+    print(result(3, 5, 1000))
+
